4b043be700462409545413251a6da239b9e36335
depanama <kudiatutomanitu@gmail.com>
2025-04-25 17:50:20 +0100

    definition de route get,patch,delete et ces differentes methodes

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -4,7 +4,10 @@ import * as notesController from "../controllers/notes"
 const router=express.Router()
 
 router.get("/",notesController.getAllNotes)
+router.get("/:noteId",notesController.getNoteById)
 router.post("/",notesController.createNote)
+router.patch("/:noteId",notesController.updateNote)
+router.delete("/:noteId",notesController.deleteNote)
 
 
 export default router
