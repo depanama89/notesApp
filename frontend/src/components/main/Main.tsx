105d1a3b77db83ced776fa544e64b8b5db548991
depanama <kudiatutomanitu@gmail.com>
2025-04-27 10:19:16 +0100

    definition des differents etats et correction de non affichage des données  et modification du composant networkapi

--- frontend/src/components/main/Main.tsx
+++ frontend/src/components/main/Main.tsx
@@ -1,36 +1,32 @@
-
 import React, { useEffect, useState } from "react";
 import Notes from "../notes/Notes";
 import { Note as NoteModel } from "../../models/notes";
-import * as NotesApi from "../../network/note_api"
+import * as NotesApi from "../../network/note_api";
 const Main = () => {
-  const [notes,setNotes]=useState<NoteModel[]>([])
+  const [notes, setNotes] = useState<NoteModel[]>([]);
 
-  const[notesLooading,setNotesLoading]=useState(true)
+  const [notesLooading, setNotesLoading] = useState(true);
 
-  useEffect(()=>{
-    async function loadNotes(){
+  useEffect(() => {
+    async function loadNotes() {
       try {
-        
-        const notes=await NotesApi.fecthNotes()
-        setNotes(notes)
+        const notes = await NotesApi.fecthNotes();
+        setNotes(notes);
       } catch (error) {
         console.log(error);
-        
       }
     }
-
-  },[])
+    loadNotes();
+  }, []);
   return (
     <div className="p-5 max-lg:p-5   flex flex-col gap-8">
       <h1 className="text-xl max-lg:text-sm  font-bold  border-b-2 border-border-color-4 pb-2 w-full ">
         Notes Epinglées
       </h1>
       <div className="grid grid-cols-1 gap-6 lg:grid-cols-3">
-      {notes.map((note)=>(
-         <Notes key={note._id} note={note}  />
-      ))}
-       
+        {notes.map((note) => (
+          <Notes key={note._id} note={note} />
+        ))}
       </div>
     </div>
   );
--- frontend/src/network/note_api.ts
+++ frontend/src/network/note_api.ts
@@ -1,5 +1,15 @@
 import {Note} from "../models/notes"
 
+// definition de l'interface pour faire passer les parametres à la methode createNote
+
+interface NoteInput{
+    title:string
+    text:string
+    category:string
+}
+// declaration de la variablr CONSTANTE API_URL
+const API_URL=process.env.REACT_APP_API_URL
+
 
 async function fecthData(url:RequestInfo,init?:RequestInit){
     const response=await fetch(url,init)
@@ -14,10 +24,37 @@ async function fecthData(url:RequestInfo,init?:RequestInit){
 }
 
 export async function fecthNotes():Promise<Note[]>{
-    // const API_URL=process.env.REACT_APP_API_URL
+    
    
-    const response = await fecthData("/api/notes",{
+    const response = await fecthData(`${API_URL}/api/notes`,{
         method:"GET"
+    })
+
+    return response.json()
+}
+
+
+
+export async function createNote(note:NoteInput):Promise<Note>{
+   const response=await fecthData(`${API_URL}/api/notes`,{
+    method:"POST",
+    headers:{
+        "content-type":"application/json"
+    },
+    body:JSON.stringify(note)
+   })
+
+   return response.json()
+}
+
+// update note
+export async function   updateNote(noteId:string,note:Note):Promise<Note>{
+    const response= await fecthData(`${API_URL}/api/notes/`+ noteId,{
+        method:"PATCH",
+        headers:{
+            "Content-type":"application/json",
+        },
+        body:JSON.stringify(note)
     })
 
     return response.json()
